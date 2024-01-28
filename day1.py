bb2a49ca832910d078dbde0aece54d14dcf6adfe
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-01-28 17:10:20 +0530

    final day 1

--- day1.py
+++ day1.py
@@ -1,2 +1,12 @@
-print("Hello World!\nThis is"+" "+input("Enter your name: ")+"❤️") 
+name = input("Enter your name: ")
+print("Hello World!\nThis is"+" "+name+"❤️  and she has "+str(len(name))+" characters in her name.") 
 # input will take input from user in console
+
+age = input("What is your current age? ") # variable age
+print(name+" is "+age+" years old.")
+
+# var names cannot be like 1name, mint@, user name -> Syntax Error
+print("Now WELCOME TO BAND NAME GENERATOR!")
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What is the name of your pet?\n")
+print("Your band name is "+ city +" "+ pet +"!")
