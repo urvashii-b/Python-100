f2ffa5e3261760d99e0cae9f02a79072b0009aca
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-01-28 23:45:49 +0530

    more

--- day2.py
+++ day2.py
@@ -13,4 +13,22 @@ print(3.14) # float
 boolz = True # boolean
 boolz = False
 
+num = input("Enter a two digit number: ")
+tens = int(num[0])
+ones = int(num[1])
+print("Sum is "+str(tens+ones))
+
+# PEMDASLR (), **, *, /, +, -
+
+print(2**3**2) # right to left-> 3**2=9, then 2**9 = 512
+height = input("Enter height in meters: ")
+weight = input("Enter weight in kilograms: ")
+
+bmi = int(weight) / (float(height)*float(height))
+print("Your BMI is: "+str(int(bmi)))
+
+print(round(8/3,4)) # round upto 4 decimal points
+print(8//3) # floor division as result is int
+
+
 
