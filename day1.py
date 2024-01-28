a1d94c8eddc0130c5b5c964ce7f680d6a0057e3f
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-01-28 20:31:16 +0530

    more

--- day1.py
+++ day1.py
@@ -5,6 +5,15 @@ print("Hello World!\nThis is"+" "+name+"❤️  and she has "+str(len(name))+" c
 age = input("What is your current age? ") # variable age
 print(name+" is "+age+" years old.")
 
+# swapping
+a = int(input("a: "))
+b = int(input("b: "))
+print("BEFORE->a: "+str(a)+" b: "+str(b))
+c = a
+a = b
+b = c
+print("AFTER SWAP->a: "+str(a)+" b: "+str(b))
+
 # var names cannot be like 1name, mint@, user name -> Syntax Error
 print("Now WELCOME TO BAND NAME GENERATOR!")
 city = input("What is the name of the city you grew up in?\n")
---
+++ day2.py
@@ -0,0 +1,16 @@
+# primitive data types
+
+name = "Urvashi" # string
+print(type(name))
+print(name+" Bhargava")
+print("12"+"34")
+
+print(12+34) # int
+print(123_456_789) # large integers
+
+print(3.14) # float
+
+boolz = True # boolean
+boolz = False
+
+
